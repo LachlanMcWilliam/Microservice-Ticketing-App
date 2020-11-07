3edc15b2b3636a96c1949bd96eaff226ebe9c787
Lachlan <mcwilliam01@gmail.com>
2020-11-07 21:43:59 +0000

    Error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/currentuser";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
+
+import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -14,6 +18,12 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("auth listening on port 3000!!");
