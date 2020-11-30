0cc51a87510cba2ff98ba49cfce5007ab9eae8aa
Lachlan <mcwilliam01@gmail.com>
2020-11-30 03:40:08 +0000

    added JWT cookie sessions

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/currentuser";
 import { signInRouter } from "./routes/signin";
@@ -12,8 +13,10 @@ import { errorHandler } from "./middleware/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 
 app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -27,6 +30,10 @@ app.all("*", () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -18,7 +19,6 @@ router.post(
       .withMessage("Password must be between 4 and 20 characters"),
   ],
   async (req: Request, res: Response) => {
-
     const errors = validationResult(req);
 
     // If errors are found in user input throw validation error
@@ -30,21 +30,35 @@ router.post(
 
     // Check if user exists
     const existingUser = await User.findOne({ email });
-    // If user already exists throw a an error 
+    // If user already exists throw a an error
     if (existingUser) {
       throw new BadRequestError("Email is already in use");
     }
 
-    //Create and save user
+    // Create and save user
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT and store it on 'session' object
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //Store the jwt in the session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     return res.status(201).send(user);
   }
 );
 
-router.get("/api/users/signup", (req, res) => {
-  res.send("hello");
-});
+// router.get("/api/users/signup", (req, res) => {
+// res.send("hello");
+// });
 
 export { router as signUpRouter };
