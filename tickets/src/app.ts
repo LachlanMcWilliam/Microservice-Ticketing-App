18df5c72ca7683888fd1c5dee76752c8f7b89643
Lachlan <mcwilliam01@gmail.com>
2020-12-20 00:50:47 +0000

    added new ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError } from "@lm-tickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@lm-tickets/common";
+
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -15,6 +17,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
